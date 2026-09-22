fa5717387d9b3ea96c68e8667d4b9e50c757e547
koperasibpkpenabur-web <koperasibpkpenabur@gmail.com>
2026-09-22 09:08:25 +0700

    chore: setup github pages deployment configuration

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import './index.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/koperasiapp">
       <UIProvider>
         <AuthProvider>
           <ProductProvider>
